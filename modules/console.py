2d2607369bbbe429827e85550da01a5e9f6595d4
Deniz <dennos746@gmail.com>
2022-11-11 21:06:18 +0300

    Update console.py

--- modules/console.py
+++ modules/console.py
@@ -1,6 +1,10 @@
+from os import system
 from colorama import Fore
 from threading import Lock
 from datetime import datetime
+from ctypes import windll
+from pystyle import Center,Colors,Colorate
+
 lock = Lock()
 
 class Console:
@@ -40,4 +44,34 @@ class Console:
         lock.acquire()
         print(f"{Fore.LIGHTBLACK_EX}[{Fore.LIGHTWHITE_EX}{datetime.strftime(datetime.now(), '%X').replace(':',f'{Fore.LIGHTBLACK_EX}:{Fore.LIGHTWHITE_EX}')}{Fore.LIGHTBLACK_EX}] {Fore.LIGHTBLACK_EX}[{Fore.LIGHTMAGENTA_EX}Humanization Completed{Fore.LIGHTBLACK_EX}]{Fore.LIGHTWHITE_EX} > {content}")
         lock.release()
+    
+class Tools:
+    
+    @staticmethod
+    def clear():
+        system('cls')
 
+    @staticmethod
+    def setTitle(threads: int, proxies: int, created: int):
+        windll.kernel32.SetConsoleTitleW(f"Seasmash Guilded Creator  |  Threads: {threads}  |  Loaded Proxies: {proxies}  |  Created: {created}  |  Made by github.com/seadhy")
+
+    @staticmethod
+    def printLogo():
+        print(Colorate.Vertical(Colors.purple_to_blue, Center.XCenter("""
+
+           ▄████████    ▄████████    ▄████████    ▄████████   ▄▄▄▄███▄▄▄▄      ▄████████    ▄████████    ▄█    █▄    
+          ███    ███   ███    ███   ███    ███   ███    ███ ▄██▀▀▀███▀▀▀██▄   ███    ███   ███    ███   ███    ███   
+          ███    █▀    ███    █▀    ███    ███   ███    █▀  ███   ███   ███   ███    ███   ███    █▀    ███    ███   
+          ███         ▄███▄▄▄       ███    ███   ███        ███   ███   ███   ███    ███   ███         ▄███▄▄▄▄███▄▄ 
+        ▀███████████ ▀▀███▀▀▀     ▀███████████ ▀███████████ ███   ███   ███ ▀███████████ ▀███████████ ▀▀███▀▀▀▀███▀  
+                 ███   ███    █▄    ███    ███          ███ ███   ███   ███   ███    ███          ███   ███    ███   
+           ▄█    ███   ███    ███   ███    ███    ▄█    ███ ███   ███   ███   ███    ███    ▄█    ███   ███    ███   
+         ▄████████▀    ██████████   ███    █▀   ▄████████▀   ▀█   ███   █▀    ███    █▀   ▄████████▀    ███    █▀   
+
+                      ⌜――――――――――――――――――――――――――――――――――――――――――――――――――――⌝
+                      ┇      [Discord] https://discord.gg/6hP3mHKSqf       ┇
+                      ┇      [Github]  https://github.com/seadhy           ┇
+                      ⌞――――――――――――――――――――――――――――――――――――――――――――――――――――⌟
+
+
+                      """)))
